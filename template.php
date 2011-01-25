9ff04cf17451b0a28007d919f6a965d1a6bc136e
mshepherd <matthew@illuminateict.org.uk>
2011-01-25 15:16:16 +0000

    Customise the search block and more styling tweaks for the upper header bar.

--- template.php
+++ template.php
@@ -36,3 +36,41 @@ function illuminate_preprocess_page() {
   rounded_corners_add_corners($commands);
   $variables['scripts'] = drupal_get_js();
 }
+function illuminate_preprocess_search_block_form(&$vars, $hook) {
+  // Note that in order to theme a search block you should rename this function
+  // to yourthemename_preprocess_search_block_form and use
+  // 'search_block_form' instead of 'search_theme_form' in the customizations
+  // bellow.
+
+  //get site name
+  $site_name = variable_get('site_name', "this website");
+  $search_string = 'Search '. strtolower($site_name);
+  
+
+  // Modify elements of the search form
+  $vars['form']['search_block_form']['#title'] = t('');
+
+  // Set a default value for the search box
+  $vars['form']['search_block_form']['#value'] = t($search_string);
+
+  // Set a default width for the search box
+  $vars['form']['search_block_form']['#size'] = 26;
+
+  // Add a custom class to the search box
+  $vars['form']['search_block_form']['#attributes'] = array(
+    'class' => 'NormalTextBox txtSearch',
+    'onfocus' => "if (this.value == '$search_string') {this.value = '';}",
+    'onblur' => "if (this.value == '') {this.value = '$search_string';}");
+
+  // Rebuild the rendered version (search form only, rest remains unchanged)
+  unset($vars['form']['search_block_form']['#printed']);
+  $vars['search']['search_block_form'] = drupal_render($vars['form']['search_block_form']);
+
+  // Rebuild the rendered version (submit button, rest remains unchanged)
+  unset($vars['form']['submit']['#printed']);
+  $vars['search']['submit'] = drupal_render($vars['form']['submit']);
+
+  // Collect all form elements to make it easier to print the whole form.
+  $vars['search_form'] = implode($vars['search']);
+}
+
